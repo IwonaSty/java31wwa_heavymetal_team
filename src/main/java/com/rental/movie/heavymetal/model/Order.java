f158fa9ad315a81b911879b27b1b13bbf51194de
MICHAL_URBANSKI <michal.urbanski@onet.pl>
2020-11-29 12:58:10 +0100

    Initial commit of my branch. Added Movie and Order class impl.

--- src/main/java/com/rental/movie/heavymetal/model/Order.java
+++ src/main/java/com/rental/movie/heavymetal/model/Order.java
@@ -1,4 +1,18 @@
 package com.rental.movie.heavymetal.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+
+    private Integer id;
+
+    private LocalDate orderDate;
+
+    private Cart cart;
 }
