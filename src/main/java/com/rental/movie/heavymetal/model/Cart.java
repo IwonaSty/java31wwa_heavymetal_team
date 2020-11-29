8a64f7e4aa5a7cdb33a3bf5c0d34d291119e680e
losu5 <losu507@tlen.pl>
2020-11-29 13:02:16 +0100

    Copy and Cart classes implementation.

--- src/main/java/com/rental/movie/heavymetal/model/Cart.java
+++ src/main/java/com/rental/movie/heavymetal/model/Cart.java
@@ -1,4 +1,15 @@
 package com.rental.movie.heavymetal.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class Cart {
+
+    private Integer id;
+    User user;
+    List<Copy> copies;
 }
--- src/main/java/com/rental/movie/heavymetal/model/Copy.java
+++ src/main/java/com/rental/movie/heavymetal/model/Copy.java
@@ -1,4 +1,16 @@
 package com.rental.movie.heavymetal.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Copy {
+
+    private Integer id;
+    private Movie movie;
+    private boolean isRented;
+    private User user;
+
+
 }
