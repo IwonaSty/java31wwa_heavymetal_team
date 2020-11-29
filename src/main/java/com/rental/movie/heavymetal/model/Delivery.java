31c9d3dc8c8b102a7d2a314d25e2b6c8b2f412c9
michalw <michalw875@gmail.com>
2020-11-29 13:00:25 +0100

    commit my Delivery and User class

--- src/main/java/com/rental/movie/heavymetal/model/Delivery.java
+++ src/main/java/com/rental/movie/heavymetal/model/Delivery.java
@@ -1,5 +1,22 @@
 package com.rental.movie.heavymetal.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Delivery {
+
+    private Integer id;
+    private LocalDate shipment_date;
+
+
 
 }
--- src/main/java/com/rental/movie/heavymetal/model/User.java
+++ src/main/java/com/rental/movie/heavymetal/model/User.java
@@ -1,4 +1,15 @@
 package com.rental.movie.heavymetal.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Integer id;
+    private String  name;
+    private String lastName;
+
 }
