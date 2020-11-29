42f84a4997756a40dec9e0b348675a7cee00dfc8
MICHAL_URBANSKI <michal.urbanski@onet.pl>
2020-11-29 11:13:19 +0100

    Initial commit

---
+++ src/main/java/com/rental/movie/heavymetal/HeavymetalApplication.java
@@ -0,0 +1,13 @@
+package com.rental.movie.heavymetal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavymetalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavymetalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rental/movie/heavymetal/HeavymetalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.movie.heavymetal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavymetalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
