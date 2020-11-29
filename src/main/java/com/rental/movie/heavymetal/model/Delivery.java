9e804fb82dfa6f8f9784c249d82052e9304c8dee
losu5 <losu507@tlen.pl>
2020-11-29 15:19:48 +0100

    Mapping realtion in progress

--- src/main/java/com/rental/movie/heavymetal/model/Delivery.java
+++ src/main/java/com/rental/movie/heavymetal/model/Delivery.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
@@ -18,8 +15,10 @@ public class Delivery {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDate shipment_date;
+    private LocalDate shipmentDate;
 
+    @OneToOne
+    private Order order;
 
 
 }
