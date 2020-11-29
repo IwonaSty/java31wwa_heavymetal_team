c8665b5867b0dac009e06b3be88c2a74122e5bf3
MICHAL_URBANSKI <michal.urbanski@onet.pl>
2020-11-29 12:58:10 +0100

    Initial commit of my branch. Added Movie and Order class impl.

--- src/main/java/com/rental/movie/heavymetal/model/Movie.java
+++ src/main/java/com/rental/movie/heavymetal/model/Movie.java
@@ -1,22 +1,28 @@
 package com.rental.movie.heavymetal.model;
 
 import com.rental.movie.heavymetal.model.rating.GlobalRating;
-import com.rental.movie.heavymetal.model.rating.Mark;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.time.LocalDate;
-
+@Data
+@NoArgsConstructor
 public class Movie {
 
-    private int id;
-    private String title;
-    private String director;
-    private Genre genre;
-    private LocalDate releaseDate;
-    private String description;
-    private GlobalRating globalrating;
+    private Integer id;
 
+    private String title;
+
+    private String director;
+
+    private Genre genre;
+
+    private LocalDate releaseDate;
+
+    private String description;
+
+    private GlobalRating globalrating;
 
 
 }
