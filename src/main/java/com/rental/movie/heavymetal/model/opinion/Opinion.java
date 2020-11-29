de59092b4ad7808abd408fb33a0324225eaa517d
Iwona Stysiak <iwona.stysiak@wp.pl>
2020-11-29 13:01:06 +0100

    add field to class Opinion- with constructor

--- src/main/java/com/rental/movie/heavymetal/model/opinion/Opinion.java
+++ src/main/java/com/rental/movie/heavymetal/model/opinion/Opinion.java
@@ -1,7 +1,14 @@
 package com.rental.movie.heavymetal.model.opinion;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Opinion {
     private String userComment;
 
