f6414f74dc8553db68130e11e500899d044d2b2e
michalw <michalw875@gmail.com>
2020-12-05 11:07:37 +0100

    commit of user and cart classes

--- src/main/java/com/rental/movie/heavymetal/repositories/UserRepository.java
+++ src/main/java/com/rental/movie/heavymetal/repositories/UserRepository.java
@@ -5,4 +5,5 @@ import com.rental.movie.heavymetal.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
 }
