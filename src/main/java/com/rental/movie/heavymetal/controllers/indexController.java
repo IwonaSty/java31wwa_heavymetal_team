2138886d58bb26dadc9a24f26a19f34c659efc5a
MICHAL_URBANSKI <michal.urbanski@onet.pl>
2020-12-06 15:27:03 +0100

    Proposal for genre movie getter

--- src/main/java/com/rental/movie/heavymetal/controllers/indexController.java
+++ src/main/java/com/rental/movie/heavymetal/controllers/indexController.java
@@ -1,13 +1,22 @@
 package com.rental.movie.heavymetal.controllers;
 
+import com.rental.movie.heavymetal.model.Genre;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class indexController {
     @GetMapping({"/",""})
     public String index() {
 
+//        List<Genre> genres = new LinkedList<>();
+//        Arrays.stream(Genre.values()).sorted().forEach(genres::add);
+//        model.addAttribute("genres", genres);
         return "index";
 
     }
