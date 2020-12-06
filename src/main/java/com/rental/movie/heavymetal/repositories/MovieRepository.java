990cfeaea4a44987c6d426c333ddc19296b0c265
Iwona Stysiak <iwona.stysiak@wp.pl>
2020-12-06 10:34:35 +0100

    add 4 movies

--- src/main/java/com/rental/movie/heavymetal/repositories/MovieRepository.java
+++ src/main/java/com/rental/movie/heavymetal/repositories/MovieRepository.java
@@ -4,8 +4,6 @@ import com.rental.movie.heavymetal.model.Genre;
 import com.rental.movie.heavymetal.model.Movie;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
 import java.util.Optional;
 
 
