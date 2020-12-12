16db44552a11e2d5c08683326dcd1f7a83a1ecba
Iwona Stysiak <iwona.stysiak@wp.pl>
2020-12-12 12:42:45 +0100

    zmiany w Enumie: Genre, mappingi w MovieController, widok dodawania nowego formularza

--- src/main/java/com/rental/movie/heavymetal/controllers/MovieController.java
+++ src/main/java/com/rental/movie/heavymetal/controllers/MovieController.java
@@ -1,11 +1,21 @@
 package com.rental.movie.heavymetal.controllers;
 
+import com.rental.movie.heavymetal.model.Genre;
+import com.rental.movie.heavymetal.model.Movie;
 import com.rental.movie.heavymetal.services.MovieService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
+@Slf4j
 @Controller
 public class MovieController {
 
@@ -29,4 +39,20 @@ public class MovieController {
         return "movies/movie";
 
     }
+
+    @GetMapping("/add-movie")
+    public String newMovie(Model model) {
+        model.addAttribute("genre", new LinkedList<Genre>(Arrays.asList(Genre.values())));
+        model.addAttribute("movie", new Movie());
+        return "movies/add-movie";
+    }
+
+    @PostMapping("/save-movie")
+    public String saveMovie(@ModelAttribute Movie movie) {
+        service.save(movie);
+        log.info("Added movie: " + movie);
+
+        return "redirect:/movies";
+    }
+
 }
--- src/main/java/com/rental/movie/heavymetal/model/Genre.java
+++ src/main/java/com/rental/movie/heavymetal/model/Genre.java
@@ -2,6 +2,20 @@ package com.rental.movie.heavymetal.model;
 
 public enum Genre {
 
-    DOCUMENTARY, HORROR, SCI_FI, FANTASY, CARTOON, ACTION, COMEDY, DRAMA, WESTERN
+    DOCUMENTARY("Documentary"),
+    HORROR("Horror"),
+    SCI_FI("Sci-fi"),
+    FANTASY("Fantasy"),
+    CARTOON("Cartoon"),
+    ACTION("Action"),
+    COMEDY("Comedy"),
+    DRAMA("Drama"),
+    WESTERN("Western"),
+    ADVENTURE ("Adventure");
 
+    private String name;
+
+    Genre(String name) {
+        this.name = name;
+    }
 }
